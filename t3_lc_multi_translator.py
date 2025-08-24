c7bdde0f6753d929d6e3059f202ef15468bd6d96
proxves7 <proxves7@gmail.com>
2025-08-24 14:12:51 +0800

    langchain translator and prompt

--- t3_lc_multi_translator.py
+++ t3_lc_multi_translator.py
@@ -2,20 +2,21 @@ import os
 from langchain.chat_models import init_chat_model
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 os.environ["GOOGLE_API_KEY"] = config.get("GOOGLE_API_KEY")
 model = init_chat_model("gemini-2.0-flash", model_provider="google_genai")
-
 parser = StrOutputParser()
 
-chain = model | parser
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", "將以下的內容翻譯為{language}"),
+     ("user", "{text}")]
+)
+chain = prompt_template | model | parser
 
+target_language = "日文"
 user_input = "知之為知之，不知為不知，是知也。"
-message = [
-    SystemMessage(content="將以下的內容翻譯為英文。"),
-    HumanMessage(content=user_input),
-]
 
-print(chain.invoke(message))
+print(chain.invoke({"language": target_language, "text": user_input}))
---
+++ t4_lc_scenario.py
@@ -0,0 +1,62 @@
+import os
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+os.environ["GOOGLE_API_KEY"] = config.get("GOOGLE_API_KEY")
+model = init_chat_model("gemini-2.0-flash", model_provider="google_genai")
+parser = StrOutputParser()
+
+system_prompt = """
+請按照這個格式回答：
+單字：{text}
+情境：{scenario}
+{native_language}翻譯： 
+{target_language}造句：
+{native_language}翻譯：
+"""
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", system_prompt),
+     ("user", "{text}")]
+)
+
+chain = prompt_template | model | parser
+
+native_language = "繁體中文"
+target_language = "英文"
+scenario_array = [
+    "在餐廳",
+    "在學校",
+    "在家裡",
+    "在辦公室",
+    "在機場",
+]
+vocabularies = [
+    "hungry",
+    # "glad",
+    # "stranger",
+    # "proud",
+    # "excited",
+]
+
+results = []
+
+for vocabulary in vocabularies:
+    for scenario in scenario_array:
+        result = chain.invoke(
+            {
+                "native_language": native_language,
+                "scenario": scenario,
+                "target_language": target_language,
+                "text": vocabulary,
+            }
+        )
+        results.append(result)
+
+for result in results:
+    print(result)
+    print("=====")
